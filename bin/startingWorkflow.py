f66fa4fb012ce9ec33685ee157b241b75023ada5
bedro <bedro@psb.ugent.be>
2019-10-29 18:09:51 +0100

    more logs

--- bin/startingWorkflow.py
+++ bin/startingWorkflow.py
@@ -11,28 +11,34 @@ library_name = 'Local data'
 history_name = 'History'
 output_history_name = 'Output history'
 outputDir = '/output'
+
+# - Incoming from WES API
 workflow_params_url = 'https://raw.githubusercontent.com/bedroesb/WES2Galaxy/master/example_data/dataMapping.json'
 workflow_url = 'https://raw.githubusercontent.com/bedroesb/WES2Galaxy/master/example_data/Galaxy-Workflow-galaxy-intro-strands-2.ga'
+workflow_version = '0.1'
+
 
 # - Create Galaxy Instance Object
 gi = GalaxyInstance(
     GALAXY_URL, email='admin@galaxy.org', password='admin')
+print('Connected to galaxy')
 
 # - Create history
 gi.histories.create_history(name=history_name)
 histories = gi.histories.get_histories(name=history_name)
 history_id = histories[0]['id']
-print('History ID: ' + history_id)
+print('History created with ID: ' + history_id)
 
 # - Create Library
 gi.libraries.create_library(name=library_name)
 libraries = gi.libraries.get_libraries(name=library_name)
 library_id = libraries[0]['id']
-print('Library ID: ' + library_id)
+print('Library created with ID : ' + library_id)
 
 # - Create folder
 gi.libraries.create_folder(library_id, 'URLdata', description=None)
 folder = gi.libraries.show_library(library_id, contents=True)[0]
+print('Folder created with ID : ' + folder['id'])
 
 # - Reading out workflow params
 datamapping_response = urllib2.urlopen(workflow_params_url)
@@ -41,6 +47,7 @@ datamapping_data = json.load(datamapping_response)
 # - Upload data in library based on URL
 for inputfile in datamapping_data['inputs']:
     gi.libraries.upload_file_from_url(library_id, inputfile['filename'], folder_id=folder['id'])
+    print(inputfile['filename'] + ' uploaded to the datalibrary')
 
 # - Load data in history
 files = gi.libraries.show_library(library_id, contents=True)
@@ -55,9 +62,11 @@ workflow_data = json.load(workflow_response)
 # - Check for installed workflow
 workflows = gi.workflows.get_workflows()
 for workflow in workflows:
-    if workflow['name'] == workflow_data['name']:
+    if workflow['name'] == workflow_data['name'] and workflow_data['format-version'] == workflow_version:
         workflow_id = workflow['id']
-print('Workflow ID: ' + workflow_id)
+        print('Workflow is available with ID: ' + workflow_id)
+    else:
+        print('This workflow is not available.')
 
 # - Examine workflow
 wf = gi.workflows.show_workflow(workflow_id)
